9bfe6a2d425c9a2aca225d04162ee06530ec79c2
Jaka Tasnim <jakatasnim85849@gmail.com>
2026-06-13 15:49:24 +0530

    redux toolkit project

--- reduxToolkitTodo/vite.config.js
+++ reduxToolkitTodo/vite.config.js
@@ -4,8 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  darkMode: "class",
-  plugins: [react(), tailwindcss()],
+  plugins: [react(),tailwindcss(),],
   
   content: [
     "./index.html",
